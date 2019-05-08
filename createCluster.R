02fbbb194f9b515da6ea0f99755feb17da2c1865
RBSalzburg <50239197+RBSalzburg@users.noreply.github.com>
2019-05-08 21:25:48 +0200

    Update createCluster.R
    error handling

--- createCluster.R
+++ createCluster.R
@@ -2,10 +2,13 @@ library(tidyverse)
 
 createCluster <- function(y,k){
   
- #todo warning message
- 
   #y matrix with columns
   #k number of groups
+  
+  if(class(y) != "matrix") warning("y is not a matrix")
+  if(class(k) != "numeric") warning("k is not a numeric vector")
+  if(any(k<0)) warning("only positiv x values are allowed") 
+  if(any(y<0)) warning("only positiv y values are allowed")
   
   groups <- round(length(y[,1])/k)
   parameters <- data.frame(mu = c(1:k),sigma2 = c(1:k))
