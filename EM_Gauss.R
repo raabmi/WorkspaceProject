f49e75b0686124256348c91027fbaa20d50b9960
Michaela Raab <raab.michaela96@gmail.com>
2019-05-08 20:00:07 +0200

    Work and Test EM - algorithm

--- EM_Gauss.R
+++ EM_Gauss.R
@@ -13,6 +13,8 @@ pjk <- function(a, b, mu, sigma2){
   # b numeric value upper bound of a bin
   # mu numeric value mean of a normal
   # sigma2 numeric value variance of a normal
+  #OUTPUT
+  # number - Area unter the density
   
   return(pnorm(b, mu, sqrt(sigma2) ) - 
            pnorm(a, mu, sqrt(sigma2)))
@@ -48,8 +50,8 @@ loglik <- function(n0, p0, J, K, pi, pjk, njk, mu, sigma2){
   # J -  numeric value Number of Bins
   # K -  numeric value Number of components
   # pi - vector of mixing proportions
-  # pjk - occurence probabilty of a jth bin in the kth density
-  # njk - vector number of expected observation in jth bin under kth density
+  # pjk - matrix (jxk) occurence probabilty of a jth bin in the kth density
+  # njk - matrix (jxk) - number of expected observation in jth bin under kth density
   # mu - vector of mean of normals
   # sigma2 - vector of variance of normals
   #OUTPUT
@@ -57,9 +59,10 @@ loglik <- function(n0, p0, J, K, pi, pjk, njk, mu, sigma2){
   
   # TODO term2 and 3
   term1 <- n0 * ln(p0)
-  term2 <- 0
-  term3 <- 0
+  term2 <- sum(colSums(njk) * log(pi))
+  term3 <- sum(njk * log(pjk)) #instead of sum sum
   
+  #Term 2
   loglik_value <- term1 + term2 + term3
   
   return(loglik_value)
@@ -73,7 +76,7 @@ loglik.pen <- function(n0, p0, J, K, pi, pjk, njk,mu, sigma2, alpha, beta){
   # K -  numeric value Number of components
   # pi - vector of mixing proportions
   # pjk - occurence probabilty of a jth bin in the kth density
-  # njk - vector number of expected observation in jth bin under kth density
+  # njk - matrix number of expected observation in jth bin under kth density
   # alpha - numeric value shape of inverse gamma distribution
   # beta  - numeric value rate of inverse gamma distribution
   #OUTPUT
@@ -84,11 +87,6 @@ loglik.pen <- function(n0, p0, J, K, pi, pjk, njk,mu, sigma2, alpha, beta){
   loglikpen <- loglik(n0, p0, J, K, pi, pjk, njk, mu, sigma2) - pen
   
   return(loglikpen)
-}
-
-tau <- function(){
-  
-  
 }
 
 # Vector of mixing proportions
@@ -117,28 +115,32 @@ njk <- function(nj, pi, mu, sigma2, a, b,  k){
   # sigma2- vector of variance of normals
   # pi - vector of mixing proportions of normals
   # k - indizes of kth component
-  
+  #OUTPUT
+  # numeric number - expected number of observations
   denum <- NULL
   for(i in 1:length(mu)){
    denum <- c(denum,
      pi[i] * pjk(a, b, mu[i], sigma2[i])
    )
-   
-   num <- nj* pi[k]* pjk(a, b, mu[k], sigma2[k])
-   return(num / sum(denum))
+
+    num <- nj* pi[k]* pjk(a, b, mu[k], sigma2[k])
   }
+  return(num / sum(denum))
+  
 }
 
 
 ########################
 ## EM - Algorithm ######
 ########################
-em.gauss <- function(y, mu, sigma2, pi, espilon=0.000001){
+em.gauss <- function(y, mu, sigma2, pi, alpha, beta, espilon=0.000001){
   # y - data numeric vector with observation per bin, 
   # n0  - first value of y must be n0
   # mu - vector of mean of normals
   # sigma2- vector of variance of normals
   # pi - vector of mixing proportions of normals
+  # alpha - numeric number alpha of inverse gauss
+  # beta - numeric number beta of inverse gauss
   # epsilon - stopping criteria (change of likelihood)
   #OUTPUT
   # list(estimated mu, estimated sigma2, liklihood value)
@@ -146,23 +148,25 @@ em.gauss <- function(y, mu, sigma2, pi, espilon=0.000001){
   
   #Initialize
   K <- length(mu) # K number of components
-  n0 <- y[1] # number of restistent observations
+  J <- length(y) # J number of Bins
+  n0 <- y[1] # number of restistant observations
+  N <- sum(y) # Total number of observations
   
   mu_est <- mu
   sigma2_est <- sigma2
   pi_est <- pi
   
   # a = lower bound of bin , b= upper bound
-  ab_bin<- data.frame(y=y, a = 5:49, b= 6: 50)
+  ab_bin<- data.frame(y=y, a = 4:49, b= 5: 50)
   
   for(i in 1:1000){
     #E- Step
     #Calculate Expected values in bin j under distribution k
     njk_exp <- matrix(0, nrow = 44, ncol= K)
     for(j in 1:44){#do it for each bin (44)
-      mjk_exp[j,] <- sapply(1:K, FUN = function(k){
-        njk (nj = y[i], 
-             pi = pi, 
+      njk_exp[j,] <- sapply(1:K, FUN = function(k){
+        njk (nj = y[j], 
+             pi = pi_est, 
              mu = mu_est, 
              sigma2 = sigma2_est, 
              a = ab_bin$a[j], 
@@ -174,17 +178,29 @@ em.gauss <- function(y, mu, sigma2, pi, espilon=0.000001){
     
     #M - Step
     
-    # Estimate tau
-    tau()
-    
     # Estimate pi
-    pi()
+    pi_est <- pi(N = N,
+                 n0 = n0,
+                 njk = njk_exp[-1, ]) #njk has to be without n0
     
-    # Optimize Liklihood (Estimate sigma, mu with optim)
+    # make a pjk matrix for likelihood
+    pjk_exp <- matrix(0, nrow = 44, ncol= K)
+    for(j in 1:44){#do it for each bin (44)
+      pjk_exp[j,] <- sapply(1:K, FUN = function(k){
+        pjk(a = ab_bin$a[k],
+            b = ab_bin$b[k],
+            mu = mu_est[k],
+            sigma2 = sigma2_est[k])
+      }) 
+    }
+    # Optimize Likelihood (Estimate sigma, mu with optim)
     
   }
   
   return(list(mu= mu_est, sigma2 =sigma2_est, loglik = loglik))
  
 }
+
+
+
 
--- Test_EM_Gauss.R
+++ Test_EM_Gauss.R
@@ -10,9 +10,9 @@ for(j in 1:44){#do it for each bin
 }
 
 njk (nj = 24, pi = c(0.4, 0.2), 
-     mu = c(5, 3), 
+     mu = c(1, 2), 
      sigma2 = c(5, 1), 
-     a = 1 , b = 2 , k = 2)
+     a = 2 , b = 3 , k = 1)
 
 sapply(1:3,FUN = function(k){
   njk (nj = 10, 
@@ -23,3 +23,53 @@ sapply(1:3,FUN = function(k){
        b = 2, 
        k = k)
 }) 
+
+y <- c(10, 5, 4, 3, 6)
+njk_exp <- matrix(0, nrow = 5, ncol= 3)
+for(j in 1:5){#do it for each bin (44)
+  njk_exp[j,] <- sapply(1:3, FUN = function(k){
+    njk (nj = y[j], 
+         pi = c(0.4, 0.4, 0.2), 
+         mu = c(1, 4, 6), 
+         sigma2 = c(1, 2, 2), 
+         a = 2, 
+         b = 3, 
+         k = k)
+  }) 
+}
+njk_exp
+
+pi(N = sum(y),
+   n0 = y[1],
+   njk = njk_exp[ -1, ])
+
+colSums(njk_exp)
+
+## Test pjk
+pjk(a = -10000, 
+    b= 2, 
+    mu = 2, 
+    sigma2 = 1) # should be 0.5
+
+#Test calculate pjk_exp
+y <- c(10, 5, 4, 3, 6)
+mu_est = c(1, 4, 6) 
+sigma2_est = c(1, 2, 2)
+ab_bin<- data.frame(y=y, a = 4:8, b= 5: 9)
+pjk_exp <- matrix(0, nrow = 5, ncol= 3)
+for(j in 1:5){#do it for each bin (44)
+  pjk_exp[j,] <- sapply(1:3, FUN = function(k){
+    pjk(a = ab_bin$a[j],
+        b = ab_bin$b[j],
+        mu = mu_est[k],
+        sigma2 = sigma2_est[k])
+  }) 
+}
+
+# Test optim
+
+optim(par, fn, 
+      method = c("Nelder-Mead", "BFGS", "CG", "L-BFGS-B", "SANN",
+                 "Brent"),
+      lower = -Inf, upper = Inf,
+      control = list(), hessian = FALSE)
